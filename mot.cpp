3052e972d8463982a0e4090b3d9664d39ab4e037
kalvinwellssss <sivibi.kalvin.nguyen@gmail.com>
2025-05-02 03:37:25 +0700

    Create mot.cpp

---
+++ mot.cpp
@@ -0,0 +1,8 @@
+/quiz-site
+├── index.html       // Page 1
+├── page2.html       // Page 2
+├── page3.html       // Page 3
+├── style.css        // Giao diện dùng chung
+├── dog.png          // Hình chó cười (hiển thị khi chọn sai)
+├── cat.png          // Hình con mèo
+├── heart.png        // Hình trái tim
